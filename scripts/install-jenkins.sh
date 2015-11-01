ac91b0a2f0b60d70de2fe3c8a85cd0d6baf50c37
Justin Menga <justin.menga@gmail.com>
2015-11-02 07:28:01 +1300

    Add -y flags

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
