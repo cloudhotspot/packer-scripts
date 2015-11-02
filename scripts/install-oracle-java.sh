a6bbc8d939bb65ce287e491f510f75303da156c0
Justin Menga <justin.menga@gmail.com>
2015-11-02 19:04:34 +1300

    Add Oracle Java 8 install script

---
+++ scripts/install-oracle-java.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update -y
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
