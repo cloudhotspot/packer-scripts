319cae119c9a9a04cc0a46d8bdfd513023387f60
Justin Menga <justin.menga@gmail.com>
2015-11-03 13:49:49 +1300

    Add make dependency

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -2,4 +2,4 @@
 wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update -y
-sudo apt-get install jenkins -y
+sudo apt-get install make jenkins -y
