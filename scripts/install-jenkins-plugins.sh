0c0636685dd41ef7f423d79015a9009e39e3a867
Justin Menga <justin.menga@gmail.com>
2015-11-11 07:28:06 +1300

    Remove Jenkins plugin script

--- scripts/install-jenkins-plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Assumes plugins.txt is available in /tmp
-sudo -u jenkins sh -c "mv /tmp/plugins.txt /usr/share/jenkins/plugins.txt"
-sudo -u jenkins sh -c "/usr/local/bin/plugins.sh /usr/share/jenkins/plugins.txt"
