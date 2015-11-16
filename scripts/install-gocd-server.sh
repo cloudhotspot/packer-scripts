010bda4528e309ab7764e8db7e1b874ffdab7e9a
Justin Menga <justin.menga@gmail.com>
2015-11-17 06:42:16 +1300

    Add make to installed packages

--- scripts/install-gocd-server.sh
+++ scripts/install-gocd-server.sh
@@ -2,4 +2,4 @@
 echo "deb http://dl.bintray.com/gocd/gocd-deb/ /" | sudo tee /etc/apt/sources.list.d/gocd.list
 wget --quiet -O - "https://bintray.com/user/downloadSubjectPublicKey?username=gocd" | sudo apt-key add -
 sudo apt-get update -y
-sudo apt-get install go-server go-agent -y
+sudo apt-get install make go-server go-agent -y
