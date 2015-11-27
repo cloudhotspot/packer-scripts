90005fe3bdc0bcbed7f31c290d99f0ad57a23531
Justin Menga <justin.menga@gmail.com>
2015-11-27 14:45:25 +1300

    Add -y flags

--- scripts/install-docker-registry.sh
+++ scripts/install-docker-registry.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget -qO- 'https://pgp.mit.edu/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add --import
-sudo apt-get update && sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install apt-transport-https -y
 echo "deb https://packages.docker.com/1.9/apt/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update && sudo apt-get install docker-engine
+sudo apt-get update && sudo apt-get install docker-engine -y
 
 # Install Docker Registry
 sudo bash -c "$(sudo docker run docker/trusted-registry install)"
